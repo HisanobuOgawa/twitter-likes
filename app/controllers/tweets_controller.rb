eb55906c327eb0b5281bd4f2136f58de2e900e13
Hisanobu Ogawa <ogawa-hisanobu@av.avex.co.jp>
2017-03-28 09:58:07 +0000

    メッセージ編集機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    @input_content = Tweet.new
+    @input_content = params[:id] ? Tweet.find(params[:id]) : Tweet.new
     @tweet = Tweet.includes(:user).order('updated_at DESC')
   end
   
@@ -8,13 +8,25 @@ class TweetsController < ApplicationController
     tweet = Tweet.new
     tweet.attributes = input_content_param
     tweet.user_id = current_user.id
-    if tweet.valid? # バリデーションチェック
+    if tweet.valid? 
       tweet.save!
     else
       flash[:alert] = tweet.errors.full_content
     end
     redirect_to action: :index
   end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.attributes = input_content_param
+    if tweet.valid? 
+      tweet.save!
+    else
+      flash[:alert] = tweet.errors.full_content
+    end
+    redirect_to action: :index
+  end
+
 
   private
   def input_content_param
